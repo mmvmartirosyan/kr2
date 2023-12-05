ad0e1e3b4aee12101a40f0e5e2e8f76039c75d18
mmvmartirosyan <147624664+mmvmartirosyan@users.noreply.github.com>
2023-12-05 22:22:25 +0300

    Update kr.js

--- kr.js
+++ kr.js
@@ -46,7 +46,7 @@ function sort(){
 
 async function getResponce() {
 
-    let responce = await fetch("shop.json")
+    let responce = await fetch("mag.json")
 
     let content = await responce.text()
     console.log(content)
@@ -81,7 +81,7 @@ async function getResponce1() {
     //let responce = await fetch("https://vmarshirov.github.io/g06u28/030_js/data/0620.json")
     //let responce = await fetch("http://185.182.111.214:7628/tmp/g06u28.txt_api.json")
 //    let responce = await fetch("shop.json")
-    let responce = await fetch("shop.json")
+    let responce = await fetch("mag.json")
 
     let content = await responce.text()
     console.log(content)
